7c07563fd94e6ec5f93516108fcba85f49087f9a
Solpo <3440817+Solpo@users.noreply.github.com>
2021-12-02 12:35:53 +0200

    eka

--- 01/eka.py
+++ 01/eka.py
@@ -1,1 +1,12 @@
-print("moi")
+with open("input_1.txt") as syote:
+    luvut = []
+    for r in syote:
+        luvut.append(int(r))
+
+kasvua = 0
+for i in range(1, len(luvut)):
+    if luvut[i] > luvut[i-1]:
+        kasvua +=1
+
+print(kasvua)
+
