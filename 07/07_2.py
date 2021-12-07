e28b2425a0fa00e2b58ec0432e2b4026dd6a4b66
Solpo <3440817+Solpo@users.noreply.github.com>
2021-12-07 17:17:55 +0200

    seiskapäivä

---
+++ 07/07_2.py
@@ -0,0 +1,15 @@
+with open("input.txt") as f:
+    asemat = [int(asema) for asema in f.readline().split(",")]
+print(asemat)
+
+pienin_bensankulutus = (-1, -1)
+for i in range(min(asemat), max(asemat) + 1):
+    bensankulutus = 0
+    for rapu in asemat:
+        bensankulutus += sum(range(abs(rapu - i), 0, -1))
+    print(f"Asemaan {i} siirtymällä kuluu bensaa yhteensä {bensankulutus} yksikköä")
+    if bensankulutus < pienin_bensankulutus[1] or pienin_bensankulutus == (-1, -1):
+        pienin_bensankulutus = (i, bensankulutus)
+print(pienin_bensankulutus)
+print(f"Pienimmällä bensankulutuksella {pienin_bensankulutus[1]} selvitään kun ravut siirtyvät asemaan {pienin_bensankulutus[0]}")
+
