51b5b51bda8020bdeab9cace9b33662f9047b735
Solpo <3440817+Solpo@users.noreply.github.com>
2021-12-07 17:56:29 +0200

    optimoitu

--- 07/07_2.py
+++ 07/07_2.py
@@ -2,7 +2,6 @@ import time
 
 with open("input.txt") as f:
     asemat = [int(asema) for asema in f.readline().split(",")]
-print(asemat)
 alkuaika = time.time()
 
 pienin_bensankulutus = (-1, -1)
@@ -11,12 +10,12 @@ for i in range(min(asemat), max(asemat) + 1):
     for rapu in asemat:
         bensankulutus += sum(range(abs(rapu - i), 0, -1))
         if bensankulutus > pienin_bensankulutus[1] and pienin_bensankulutus != (-1, -1):
-            print(f"Meni yli, asemaan {i} siirtymällä bensaa kuluu yli {pienin_bensankulutus[1]} yksikköä")
+            # print(f"Meni yli, asemaan {i} siirtymällä bensaa kuluu yli {pienin_bensankulutus[1]} yksikköä")
             break
     else:
-        print(f"Asemaan {i} siirtymällä kuluu bensaa yhteensä {bensankulutus} yksikköä")
+    # print(f"Asemaan {i} siirtymällä kuluu bensaa yhteensä {bensankulutus} yksikköä")
         if bensankulutus < pienin_bensankulutus[1] or pienin_bensankulutus == (-1, -1):
             pienin_bensankulutus = (i, bensankulutus)
-print(pienin_bensankulutus)
+
 print(f"Pienimmällä bensankulutuksella {pienin_bensankulutus[1]} selvitään kun ravut siirtyvät asemaan {pienin_bensankulutus[0]}")
 print(f"Aikaa kului {time.time() - alkuaika}")
